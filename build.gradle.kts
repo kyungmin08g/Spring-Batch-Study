6d85824faddfa066c42e35e6f11bafc3f9a4630d
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-10-12 17:27:57 +0900

    chore: Initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+  kotlin("jvm") version "1.9.25"
+  kotlin("plugin.spring") version "1.9.25"
+  kotlin("plugin.jpa") version "1.9.25"
+  id("org.springframework.boot") version "3.5.6"
+  id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.kyungmin"
+version = "0.0.1-SNAPSHOT"
+description = "Spring-Batch-Study"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+configurations {
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+  // Spring Batch
+  implementation("org.springframework.boot:spring-boot-starter-batch")
+
+  // Kotlin JDSL
+  implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.5.7")
+  implementation("com.linecorp.kotlin-jdsl:jpql-render:3.5.7")
+  implementation("com.linecorp.kotlin-jdsl:spring-data-jpa-support:3.5.7")
+
+  compileOnly("org.projectlombok:lombok")
+  runtimeOnly("com.mysql:mysql-connector-j")
+  annotationProcessor("org.projectlombok:lombok")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+  testImplementation("org.springframework.batch:spring-batch-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
+}
+
+allOpen {
+  annotation("jakarta.persistence.Entity")
+  annotation("jakarta.persistence.MappedSuperclass")
+  annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/kyungmin/springbatchstudy/SpringBatchStudyApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.kyungmin.springbatchstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+  @Test
+  fun contextLoads() { }
+}
