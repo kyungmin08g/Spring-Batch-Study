fcc3dafb195e0330966036833e3f0d195d4fa05a
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-10-14 18:48:17 +0900

    docs: add detailed code comments for better understanding

--- src/main/kotlin/com/kyungmin/springbatchstudy/batch/table_to_table/entity/AfterEntity.kt
+++ src/main/kotlin/com/kyungmin/springbatchstudy/batch/table_to_table/entity/AfterEntity.kt
@@ -9,13 +9,11 @@ import jakarta.persistence.Table
 import lombok.AllArgsConstructor
 import lombok.Getter
 import lombok.NoArgsConstructor
-import lombok.Setter
 
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Setter
 @Table(name = "after_entity")
 class AfterEntity(
 
