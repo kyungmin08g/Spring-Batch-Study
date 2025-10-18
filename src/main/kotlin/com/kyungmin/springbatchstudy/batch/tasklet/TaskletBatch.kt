59b10cd514e0c2255c646b55dd5f381a75a91a4c
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-10-18 18:15:58 +0900

    docs: add comments to Tasklet batch code

--- src/main/kotlin/com/kyungmin/springbatchstudy/batch/tasklet/TaskletBatch.kt
+++ src/main/kotlin/com/kyungmin/springbatchstudy/batch/tasklet/TaskletBatch.kt
@@ -11,6 +11,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
 
+/**
+ * Tasklet 방식 Batch Config Class
+ */
+
 @Configuration
 class TaskletBatch(
   private val jobRepo: JobRepository,
@@ -27,9 +31,19 @@ class TaskletBatch(
   fun taskletStep(): Step = StepBuilder("taskletStep", jobRepo)
     .tasklet(
       { contribution, chunkContext ->
-        println("Tasklet 방식")
-        RepeatStatus.FINISHED
+        println("Tasklet 방식") // 비즈니스 로직 부분
+        RepeatStatus.FINISHED // 작업이 완료됨을 알려줌.
       },
-      transactionManager
+      transactionManager // Transaction Manager
     ).build()
 }
+
+/**
+ * 실행결과
+ * Job: [SimpleJob: [name=taskletJob]] launched with the following parameters: [{'value':'{value=a, type=class java.lang.String, identifying=true}'}]
+ * Executing step: [taskletStep]
+ * 출력 -> Tasklet 방식
+ * Step: [taskletStep] executed in 21ms
+ * Job: [SimpleJob: [name=taskletJob]] completed with the following parameters: [{'value':'{value=a, type=class java.lang.String, identifying=true}'}]
+ * and the following status: [COMPLETED] in 66ms
+ */
